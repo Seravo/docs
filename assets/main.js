65f5fd59c22a14a71c8cd74930ce1d8456793154
Otto Kekäläinen <otto@seravo.fi>
2019-04-23 11:05:35 +0300

    Show banner promoting trainings to Finnish visitors

--- assets/main.js
+++ assets/main.js
@@ -25,4 +25,25 @@
       noResultsText: 'No results found, Sorry! Please try again or add an issue <a href="https://github.com/{{site.github.repo}}/issues/">in Github</a>.'
     });
   });
+
+  // Show trainings banner for Finnish visitors
+  if ( localStorage.getItem('dev-training-banner') != 'closed' &&
+       (
+         jQuery.inArray('fi', window.navigator.languages) > -1 ||
+         jQuery.inArray('fi-FI', window.navigator.languages) > -1
+       )
+     ) {
+    $("#dev-training-banner").show();
+  } else {
+    $("#dev-training-banner").hide();
+  }
+
+  // Enable closing banner if it annoys
+  $('#dev-training-banner .close').click( function (e){
+    $("#dev-training-banner").hide();
+    // Store selection and never show the banner again
+    localStorage.setItem('dev-training-banner', 'closed');
+    // Use localStorage.clear(); to reset selection
+  });
+
 })(jQuery);
